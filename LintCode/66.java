e7f529d7b2ffe7ba2e9d6a786d91628322c784ba
Thea <thea@v1040-wn-rt-c-80-226.campus-dynamic.uwaterloo.ca>
2019-10-03 11:26:14 -0400

    lintcode Q66

---
+++ LintCode/66.java
@@ -0,0 +1,56 @@
+/**
+66. Binary Tree Preorder Traversal
+Given a binary tree, return the preorder traversal of its nodes' values.
+
+Example
+Example 1:
+
+Input：{1,2,3}
+Output：[1,2,3]
+Explanation:
+   1
+  / \
+ 2   3
+it will be serialized {1,2,3}
+Preorder traversal
+Example 2:
+
+Input：{1,#,2,3}
+Output：[1,2,3]
+Explanation:
+1
+ \
+  2
+ /
+3
+it will be serialized {1,#,2,3}
+Preorder traversal
+
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param root: A Tree
+     * @return: Preorder in ArrayList which contains node values.
+     */
+    public List<Integer> preorderTraversal(TreeNode root) {
+        // write your code here
+        List<Integer> list = new ArrayList<>();
+        if (root == null){
+            return list;
+        }
+        list.add(root.val);
+        list.addAll(preorderTraversal(root.left));
+        list.addAll(preorderTraversal(root.right));
+        return list;
+    }
+}
